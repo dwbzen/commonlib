945eb904ad9df4b888c9317273c219268ae525ba
dwbzen <dwbzen@gmail.com>
2019-03-08 16:07:18 -0700

    Minor changes.

--- src/main/java/mathlib/ifs/LinearFunction.java
+++ src/main/java/mathlib/ifs/LinearFunction.java
@@ -1,5 +1,6 @@
 package mathlib.ifs;
 
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.math.MathContext;
 import java.util.ArrayList;
@@ -7,8 +8,14 @@ import java.util.Date;
 import java.util.List;
 import java.util.function.Function;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import mathlib.Matrix;
 import mathlib.Point2D;
@@ -33,24 +40,51 @@ import mathlib.util.IJson;
 public class LinearFunction implements IJson, Function<Point2D<Double>, Point2D<Double>>  {
 	
 	private static final long serialVersionUID = -4451686768008927428L;
+	protected static final Logger log = LogManager.getLogger(LinearFunction.class);
+
 	public static final String ObjectType = "LinearFunction";
 	public static final MathContext mathContext = IteratedFunctionSystem.mathContext;
+	static ObjectMapper mapper = new ObjectMapper();
 	
 	public static final BigDecimal lowerLimit = IteratedFunctionSystem.lowerLimit;	// any number having an absolute value <= lowerLimit is set to 0.0
 	
+	@JsonProperty			private String name;
 	@JsonProperty("type")	private String type = ObjectType;
 	//  the function Matrix must be 2 x 3
 	@JsonProperty			private  Matrix<BigDecimal> function = null;
 	@JsonProperty("weight")	private double weight = 0;
-	@JsonIgnore				private String name;
 	@JsonIgnore	private List<Variation>	variations = new ArrayList<Variation>();
 
 	public static void main(String[] args) {
-		double[][] dm3 = { {.5, 0, 0}, {0, .5, .5} };
+		double[][] dm3 =  { {0.0, 0.65, 0.85}, {0.646604, 0.009, 0.54} };
 		LinearFunction f1 = new LinearFunction(dm3);
 		f1.setName("f1");
+		f1.setWeight(0.5);
 		System.out.println(f1);
 		System.out.println(f1.toJson());
+		
+		String jsonstr = args[0];
+		System.out.println(jsonstr);
+		LinearFunction lf = null;
+		IteratedFunctionSystem ifs = new IteratedFunctionSystem();
+		try {
+			lf = mapper.readValue(jsonstr, LinearFunction.class);
+		} catch (JsonParseException e) {
+			log.error("JsonParseException (Point2D): " + jsonstr);
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			log.error("JsonMappingException (Point2D): " + jsonstr);
+			e.printStackTrace();
+		} catch (IOException e) {
+			log.error("IOException: " + jsonstr);
+			e.printStackTrace();
+		}
+		Point2D<Double> point = ifs.getRandomPoint();
+		System.out.println(point.toString());
+		for(int i=0; i<20; i++) {
+			point = lf.apply(point);
+			System.out.println(point.toString());
+		}
 	}
 	
 	public LinearFunction() {
