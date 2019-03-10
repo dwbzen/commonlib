17d6468e77f662d8df80c8d76d2e7c1424144184
ZENALIEN2013\don_bacon <dwbzen@gmail.com>
2019-03-10 12:42:29 -0400

    Fixed tests

--- src/test/java/junit/CommandMessageTest.java
+++ src/test/java/junit/CommandMessageTest.java
@@ -1,35 +1,30 @@
 package junit;
 
 import junit.framework.TestCase;
-import mathlib.BaseJsonObject;
 import mathlib.CommandMessage;
 
 public class CommandMessageTest   extends TestCase {
 
 	public void testToJson() {
+		//{"name":"ifs2","type":"message","command":"START"}
 		String sample = " {\"name\" : \"ifs2\", \"type\" : \"message\", \"command\" : \"SHUTDOWN\" }";
 		CommandMessage cm = CommandMessage.fromJSONString(sample);
-		String expected = "{\"name\": \"ifs2\",\"type\": \"message\",\"command\": \"SHUTDOWN\" }";
+		String expected = "{\"name\":\"ifs2\",\"type\":\"message\",\"command\":\"SHUTDOWN\"}";
 		assertEquals(expected, cm.toJson());
 	}
 	
 	public void testFromJson() {
+		//{"name":"ifs2","type":"message","command":"START"}
 		String sample2 = " {\"name\" : \"ifs2\", \"type\" : \"message\", \"command\" : \"START\" }";
 		CommandMessage cm = CommandMessage.fromJSONString(sample2);
-		String expected = "{\"name\": \"ifs2\",\"type\": \"message\",\"command\": \"START\" }";
+		String expected = "{\"name\":\"ifs2\",\"type\":\"message\",\"command\":\"START\"}";
 		assertEquals(expected, cm.toJson());
 	}
 	
 	public void testCreateCommandMessage() {
 		CommandMessage cmShutdown = new CommandMessage("test", "SHUTDOWN");
-		String expected = "{\"name\": \"test\",\"type\": \"message\",\"command\": \"SHUTDOWN\" }";
+		String expected = "{\"name\":\"test\",\"type\":\"message\",\"command\":\"SHUTDOWN\"}";
 		assertEquals(expected, cmShutdown.toJson());
 	}
-	
-	public void testBsonObject() {
-		String sample = " { \"_id\" : \"52b30e7eba2912447205bd4e\", \"name\" : \"ifs2\", \"type\" : \"unknown\" }";
-		BaseJsonObject cm = BaseJsonObject.fromJson(sample);
-		String expected = "{_id:52b30e7eba2912447205bd4e,name:\"ifs2\",type:\"unknown\" }";
-		assertEquals(expected, cm.toJson());
-	}
+
 }
