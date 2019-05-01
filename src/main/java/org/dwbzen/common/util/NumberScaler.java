c30c2ac7af51e14c83d30cadd2942ca7ed9223eb
dbacon <dbacon@connectwise.com>
2019-05-01 17:46:14 -0400

    Updates

--- src/main/java/org/dwbzen/common/util/NumberScaler.java
+++ src/main/java/org/dwbzen/common/util/NumberScaler.java
@@ -24,7 +24,9 @@ public class NumberScaler {
 	private int xSpan = 0;
 	private OrderedPair<Integer, Integer> yRange = null;
 	private int ySpan = 0;
-	public static MathContext mathContext = new MathContext(4, RoundingMode.HALF_DOWN);
+	private int precision = 4;	// decimal places
+	private MathContext mathContext = new MathContext(precision, RoundingMode.HALF_DOWN);
+	private BigDecimal bd = null;	// for Rounding
 	
 	public NumberScaler(PointSetStats<Double> stats, OrderedPair<Integer, Integer> xrange, OrderedPair<Integer, Integer> yrange) {
 		pointSetStats = new PointSetStats<>(stats);
@@ -38,8 +40,13 @@ public class NumberScaler {
 	
 	public Point2D<Integer> scale(Point2D<Double> point) {
 		Point2D<Integer> scaledPoint = new Point2D<Integer>(0,0);
-		
+		double xs = (point.getX() / xPointSpan * xSpan) + xRange.getFirst();
 		return scaledPoint;
+	}
+	
+	private int round(double num) {
+		bd = new BigDecimal(num);
+		return bd.round(mathContext).intValue();
 	}
 
 	public PointSetStats<Double> getPointSetStats() {
@@ -54,11 +61,29 @@ public class NumberScaler {
 		return yRange;
 	}
 	
+	public int getPrecision() {
+		return precision;
+	}
+
+	public void setPrecision(int precision) {
+		this.precision = precision;
+		mathContext =  new MathContext(precision, RoundingMode.HALF_DOWN);
+	}
+
+	public MathContext getMathContext() {
+		return mathContext;
+	}
+
 	public static void main(String...args) {
-		Double d = 370.49;
-		BigDecimal bd = new BigDecimal(d);
-		BigDecimal r = bd.round(mathContext);
-		System.out.println(d + " " + r + " " + r.intValue());
+		PointSetStats<Double> stats = new PointSetStats<>(0.02508, 1.991, 0.2074, 1.84, 
+				new Point2D<Double>(0.02508, 0.2074), new Point2D<Double>(1.924, 1.803));
+		OrderedPair<Integer, Integer> xRange = new OrderedPair<>(0, 1280);
+		OrderedPair<Integer, Integer> yRange = new OrderedPair<>(0, 1024);
+		
+		NumberScaler scaler = new NumberScaler(stats, xRange, yRange);
+		Point2D<Double> point = new Point2D<>(1.745, 0.7435);	// scale this
+		Point2D<Integer> scaledPoint = scaler.scale(point);
+		System.out.println(scaledPoint.toJson(true));
 	}
 	
 }
